e045126c4aaf6845231d258daf2b669929bba4d8
Hajime Taniguchi <hameji@gmail.com>
2021-12-19 06:50:16 -0500

    xcodeでプロジェクトを作成、gitignoreを設置

---
+++ iOS/MichiEki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MichiEki
+//
+//  Created by Hajime Taniguchi on 2021/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
