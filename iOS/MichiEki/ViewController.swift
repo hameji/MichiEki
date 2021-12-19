3e77bfd7b86e855d0e53bf2e28dbcfa870d4414a
Hajime Taniguchi <hameji@gmail.com>
2021-12-19 07:00:28 -0500

    viewControllerにoutletを接続

--- iOS/MichiEki/ViewController.swift
+++ iOS/MichiEki/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
