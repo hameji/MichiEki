66073444a5e673364fab1262bbcf1b8fa4b24271
Hajime Taniguchi <hameji@gmail.com>
2021-12-20 01:44:46 -0500

    protocol LocationServiceを作成

---
+++ iOS/MichiEki/LocationService.swift
@@ -0,0 +1,28 @@
+//
+//  LocationService.swift
+//  MichiEki
+//
+//  Created by Hajime Taniguchi on 2021/12/19.
+//
+
+protocol LocationService {
+
+    // MARK: - Type Aliases
+    typealias AddressLocationDataResult = (Result<[Location], LocationServiceError>) -> Void
+    typealias EkiLocationDataResult = (Result<[EkiLocation], LocationServiceError>) -> Void
+
+    // MARK: - Methods
+    func getCoordinate(address: String, completion: @escaping AddressLocationDataResult)
+
+}
+
+// MARK: - Types
+
+enum LocationServiceError: Error {
+    
+    // MARK: - Cases
+    
+    case invalidAddressString
+    case requestFailed(Error)
+    
+}
