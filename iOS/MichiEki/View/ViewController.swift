d6229106cef697ef34d8c575734c5836c70b0c26
Hajime Taniguchi <hameji@gmail.com>
2021-12-22 01:37:58 -0500

    viewModelを利用するようにviewControllerを変更

--- iOS/MichiEki/View/ViewController.swift
+++ iOS/MichiEki/View/ViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-
-    let searchController = SearchController()
-
+    
+    private var mapViewModel: MapViewModel!
+    
     @IBOutlet weak var searchBar: UISearchBar! {
         didSet {
             searchBar.delegate = self
@@ -23,16 +23,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapViewModel = MapViewModel()
     }
     
     private func addPin(facility: [EkiLocation]) {
-        if facility.count == 1 { return }
         facility.forEach {
-            let pin = MKPointAnnotation()
-            pin.title = $0.name
-            pin.subtitle = "lati:\($0.lati), long:\($0.long)"
-            let coordinate = CLLocationCoordinate2D(latitude: $0.lati, longitude: $0.long)
-            pin.coordinate = coordinate
+            let pin = mapViewModel.convertPin(location: $0)
             mapView.addAnnotation(pin)
         }
     }
@@ -47,35 +43,15 @@ extension ViewController: UISearchBarDelegate {
         searchBar.resignFirstResponder()
         guard let searchWord = searchBar.text,
               !searchWord.isEmpty else { return }
-        search(keyword: searchWord)
-    }
-    
-    private func search(keyword: String) {
-        searchController.getCoordinate(address: keyword) { [weak self] result in
+        mapViewModel.search(keyword: searchWord) { [weak self] result in
             switch result {
             case .failure(let error):
-                print(error)
+                print(error.localizedDescription)
             case .success(let locations):
-                locations.forEach {
-                    self?.searchController.getFacility(location: $0) { [weak self] result in
-                        switch result {
-                        case .failure(let error):
-                            print(error)
-                        case .success(let locations):
-                            guard !locations.isEmpty else {
-                                return
-                            }
-                            let latSorted:[Double] = locations.map{ $0.lati }.sorted(by: { $0 > $1 })
-                            let longSorted:[Double] = locations.map{ $0.long }.sorted(by: { $0 > $1 })
-                            let averLong = (longSorted.first! + longSorted.last!) / 2
-                            let averLati = (latSorted.first! + latSorted.last!) / 2
-                            let center = CLLocationCoordinate2D(latitude: averLati,
-                                                                longitude: averLong)
-                            self?.setCenter(coordinate: center)
-                            self?.addPin(facility: locations)
-                        }
-                    }
-                }
+                guard let self = self else { return }
+                let center = self.mapViewModel.getCenter(locations: locations)
+                self.setCenter(coordinate: center)
+                self.addPin(facility: locations)
             }
         }
     }
